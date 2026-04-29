f16c4ee8019b67eb9b7172b0a9fd9546272cc41d
chaeha1028 <chaeha1028@duksung.ac.kr>
2026-04-29 17:12:11 +0900

    change-1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
+let px, py; //팩맨위치
+let pd; //팩맨크디
+
+let dx; //콩위치
+let dy;
+let dActive; //콩생사여부
+let sSize; //콩크기
+
+let wx; //벽위치
+let wy;
+let ww = 100; //벽넓이
+let wh = 100; //벽높이
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
