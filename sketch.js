1c50b6036f1a952411b1df96ed1df6502732664b
chaeha1028 <chaeha1028@duksung.ac.kr>
2026-04-29 17:52:10 +0900

    change-6

--- sketch.js
+++ sketch.js
@@ -22,18 +22,22 @@ function draw() {
   //위쪽 벽
   drawWall(50, 70, 310, 35);
   drawWall(420, 70, 310, 35);
-  drawWall(50, 70, 35, 130);
-  drawWall(725, 70, 35, 130);
+  drawWall(50, 70, 35, 175);
+  drawWall(725, 70, 35, 175);
+
   //아래쪽 벽
   drawWall(50, 500, 700, 35);
-  drawWall(50, 415, 35, 120);
-  drawWall(725, 415, 35, 120);
+  drawWall(50, 370, 35, 165);
+  drawWall(725, 370, 35, 165);
 
   //위쪽 중간 홈
-  drawWall(360, 40, 25, 80);
-  drawWall(415, 40, 25, 80);
-  drawWall(360, 120, 80, 25);
+  drawWall(360, 70, 25, 80);
+  drawWall(415, 70, 25, 80);
+  drawWall(360, 150, 80, 25);
 
-  //좌우 중간 홈
   //좌우 워프 통로
+  drawWall(0, 245, 85, 30);
+  drawWall(0, 340, 85, 30);
+  drawWall(725, 245, 85, 30);
+  drawWall(725, 340, 85, 30);
 }
