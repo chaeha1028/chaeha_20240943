8f0c831fcd9e107e648bc2dbc06db90ff1608770
chaeha1028 <chaeha1028@duksung.ac.kr>
2026-04-29 17:29:01 +0900

    chanege-3

--- sketch.js
+++ sketch.js
@@ -4,12 +4,11 @@ let pd; //팩맨크디
 let dx; //콩위치
 let dy;
 let dActive; //콩생사여부
-let sSize; //콩크기
+let dSize; //콩크기
 
-let wx = 100; //벽위치
-let wy = 100;
-let ww = 300; //벽넓이
-let wh = 15; //벽높이
+function drawWall(wx, wy, ww, wh){
+  rect(wx, wy, ww, wh);
+} //벽그리기함수
 
 function setup() {
   createCanvas(800, 600);
@@ -18,6 +17,6 @@ function setup() {
 function draw() {
   background(0);
 
-  fill(0, 0, 275);
+  fill(0, 0, 180);
   rect(wx, wy, ww, wh);
 }
